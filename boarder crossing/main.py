7d31ce96855c7b614a03c6cc721a9826c760c12e
Shubham Dankhara <sdankhara1@gmail.com>
2022-11-18 14:22:34 +0530

    Add new scripts

--- boarder crossing/main.py
+++ boarder crossing/main.py
@@ -6,4 +6,4 @@ sys.path.append('S:/python project by MR.SD/boarder crossing/port_entries')
 import port_entries.find
 
 
-print(port_entries.find.port_by_state('Idaho'))
+print(port_entries.find.port_by_name('Andrade'))
